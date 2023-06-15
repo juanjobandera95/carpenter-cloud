3f425f09cfcdc97576e05bc92bd93b818db2ac53
banderajj1995@gmail.com <banderajj1995@gmail.com>
2023-06-15 23:42:54 +0200

    changes

--- src/main/java/com/carpentery/springboot/controller/ApiController.java
+++ src/main/java/com/carpentery/springboot/controller/ApiController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 	 @GetMapping("/hello")
 	    public String sayHello() {
-	        return "Hello, World!";
+	        return "Hello, World!dfdsfsd";
 	    }
 }
