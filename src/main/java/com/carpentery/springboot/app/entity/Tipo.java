786114f7a7cef9a742011e1d485bd5f204e4b162
banderajj1995@gmail.com <banderajj1995@gmail.com>
2023-06-20 17:38:49 +0200

    entities

--- src/main/java/com/carpentery/springboot/app/entity/Tipo.java
+++ src/main/java/com/carpentery/springboot/app/entity/Tipo.java
@@ -65,23 +65,5 @@ public class Tipo {
 	}
 
 
-	public int getTypeId() {
-		return typeId;
-	}
-
-
-	public void setTypeId(int typeId) {
-		this.typeId = typeId;
-	}
-
-
-	public int getCategoryId() {
-		return categoryId;
-	}
-
-
-	public void setCategoryId(int categoryId) {
-		this.categoryId = categoryId;
-	}
-
+	
 }
