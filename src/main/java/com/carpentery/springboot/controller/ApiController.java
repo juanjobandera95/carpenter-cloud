bca85d806827f0ba5b745ded6a3fe8aeb6bdf2bc
banderajj1995@gmail.com <banderajj1995@gmail.com>
2023-10-19 00:50:49 +0200

    relaciones tipos-categorias

--- src/main/java/com/carpentery/springboot/controller/ApiController.java
+++ src/main/java/com/carpentery/springboot/controller/ApiController.java
@@ -4,18 +4,30 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.carpentery.springboot.app.model.CategoryModel;
 import com.carpentery.springboot.app.model.JobModel;
+import com.carpentery.springboot.app.model.TipoModel;
+import com.carpentery.springboot.app.repository.CategoryRepository;
 import com.carpentery.springboot.app.repository.JobRepository;
+import com.carpentery.springboot.app.repository.TipoRepository;
 @RestController
 public class ApiController {
 	
     @Autowired
     private JobRepository jobRepository;
-	 @GetMapping("/jobs")
-	   public Iterable<JobModel>getAll() {
+	 
+    @Autowired
+    private CategoryRepository categoryRepository;
+	 
+    @Autowired
+    private TipoRepository tipoRepository;
+	 
+    
+    
+    @GetMapping("/jobs")
+	   public List<JobModel> getAll() {
 		 List<JobModel> jobs = jobRepository.findAll();
 	        // Personalizar las respuestas JSON para incluir el nombre de la categoría y el tipo
 	        jobs.forEach(job -> {
@@ -26,4 +38,20 @@ public class ApiController {
 	        return jobs;
 	 }
 	 
+    @GetMapping("/categories")
+	   public List<CategoryModel> getAllCategories() {
+		 List<CategoryModel> cat = categoryRepository.findAll();
+	        
+	        
+	        return cat;
+	 }
+    
+    
+    @GetMapping("/types")
+	   public List<TipoModel> getAlltypes() {
+		 List<TipoModel> types = tipoRepository.findAll();
+	        
+	        
+	        return types;
+	 }
 }
